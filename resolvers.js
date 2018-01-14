5ea85ff5ded99a343a047e1634cbe62bfa3e6551
Cesar William <cesarwbr@gmail.com>
2018-01-14 21:02:11 -0200

    Change API

--- resolvers.js
+++ resolvers.js
@@ -1,68 +1,93 @@
 import fetch from 'node-fetch'
 import { PubSub, withFilter } from 'graphql-subscriptions'
 
-const BASE_URL = 'https://min-api.cryptocompare.com/data'
+const BASE_URL = 'https://www.bitstamp.net/api/v2/ticker'
 
 const pubsub = new PubSub()
 
-global.setInterval(() => {
-	fetch(`${BASE_URL}/pricemulti?fsyms=BTC&tsyms=USD`)
-	.then(res => res.json())
-	.then(result => {
-		const coins = result
+const coinsObj = [
+  {
+    id: 'btcusd',
+    name: 'BTC',
+    displayName: 'Bitcon',
+    image:
+      'https://seeklogo.com/images/B/bitcoin-logo-DDAEEA68FA-seeklogo.com.png'
+  },
+  {
+    id: 'xrpusd',
+    name: 'XRP',
+    displayName: 'Ripple',
+    image:
+      'https://www.cryptosaurus.cc/ctrl/wp-content/uploads/RIPPLE.png'
+  },
+  {
+    id: 'ltcusd',
+    name: 'LTC',
+    displayName: 'Litecoin',
+    image:
+      'https://www.techaroha.com/wp-content/uploads/2017/09/litecoin.png'
+  },
+  {
+    id: 'ethusd',
+    name: 'ETH',
+    displayName: 'Ethereum',
+    image: 'https://www.ethereum.org/images/logos/ETHEREUM-ICON_Black.png'
+  }
+]
 
-		const coin = {
-			name: 'BTC',
-			price: coins.BTC.USD
-		}
+function listenToPrice(coinObj) {
+  global.setInterval(() => {
+    fetch(`${BASE_URL}/${coinObj.id}`)
+      .then(res => res.json())
+      .then(result => {
+        const coins = result
 
-		pubsub.publish('priceChanged', { priceChanged: coin, name: coin.name })
-	})
-}, 10000)
+        const coin = {
+          name: coinObj.name,
+          price: coins.last
+        }
 
-global.setInterval(() => {
-	fetch(`${BASE_URL}/pricemulti?fsyms=ETH&tsyms=USD`)
-	.then(res => res.json())
-	.then(result => {
-		const coins = result
+        pubsub.publish('priceChanged', { priceChanged: coin, name: coin.name })
+      })
+  }, 10000)
+}
 
-		const coin = {
-			name: 'ETH',
-			price: coins.ETH.USD
-		}
-
-		pubsub.publish('priceChanged', { priceChanged: coin, name: coin.name })
-	})
-}, 5000)
+coinsObj.forEach(coinObj => listenToPrice(coinObj))
 
 export const resolvers = {
-	Query: {
-		coins: () => {
-			return fetch(`${BASE_URL}/pricemulti?fsyms=BTC,ETH&tsyms=USD,BRL`)
-			.then(res => res.json())
-			.then(result => {
-				const coins = result
+  Query: {
+    coins: () => {
+			const promises = coinsObj.map(coinObj => {
+				return fetch(`${BASE_URL}/${coinObj.id}`)
+      })
 
-				return [{
-					name: 'BTC',
-					displayName: 'Bitcon',
-					price: coins.BTC.USD,
-					image: 'https://seeklogo.com/images/B/bitcoin-logo-DDAEEA68FA-seeklogo.com.png'
-				}, {
-					name: 'ETH',
-					displayName: 'Ethereum',
-					price: coins.ETH.USD,
-					image: 'https://www.ethereum.org/images/logos/ETHEREUM-ICON_Black.png'
-				}]
+      return Promise.all(promises)
+			.then(results => {
+				return Promise.all(results.map(res => res.json()))
 			})
-		}
-	},
-	Subscription: {
-		priceChanged: {
-			subscribe: withFilter(() => pubsub.asyncIterator('priceChanged'), (payload, variables) => {
-				console.log('payload', payload, 'variables', variables)
-				return payload.name === variables.name
-			})
-		}
-	}
+			.then(coins => {
+				console.log('coins', coins)
+				return coins.map((coin, index) => {
+					console.log('coin', coin)
+					return ({
+            name: coinsObj[index].name,
+            displayName: coinsObj[index].displayName,
+            price: parseFloat(coin.last),
+            image: coinsObj[index].image
+					})
+				})
+        })
+    }
+  },
+  Subscription: {
+    priceChanged: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator('priceChanged'),
+        (payload, variables) => {
+          console.log('payload', payload, 'variables', variables)
+          return payload.name === variables.name
+        }
+      )
+    }
+  }
 }
